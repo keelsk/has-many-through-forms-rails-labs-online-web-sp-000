2ef075619e6332a397e96ad871e2c8bfb4bb1162
Kirsten Keels <keelsk@gmail.com>
2020-04-27 15:07:06 +0000

    Automatically backed up by Learn

--- app/models/post.rb
+++ app/models/post.rb
@@ -7,7 +7,7 @@ class Post < ActiveRecord::Base
   def categories_attributes=(categories_hashes)
     categories_hashes.each do |index, category_attributes|
       category = Category.find_or_create_by(name: category_attributes[:name])
-      self.categories << category
+      self.post_categories.build(category: category)
     end
   end
 end
