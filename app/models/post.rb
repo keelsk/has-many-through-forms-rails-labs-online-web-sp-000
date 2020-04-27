4b65e8148d9edcdf7c29619aac22b303272727d9
Kirsten Keels <keelsk@gmail.com>
2020-04-27 15:19:02 +0000

    Automatically backed up by Learn

--- app/models/post.rb
+++ app/models/post.rb
@@ -4,7 +4,7 @@ class Post < ActiveRecord::Base
   has_many :comments
   has_many :users, through: :comments
 
-  accepts_nested_attributes_for :categories, reject_if: :all_blank
+  accepts_nested_attributes_for :category, reject_if: :all_blank
 
   def categories_attributes=(categories_hashes)
     categories_hashes.each do |index, category_attributes|
